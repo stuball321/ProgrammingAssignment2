790191f4e21ff358b2f152a376a2be5325bbda07
LIMS033 <stuart.lim@disney.com>
2015-06-15 16:42:32 -0700

    First attempt at assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# Create the matrix object that contains the function getters and setters
 makeCacheMatrix <- function(x = matrix()) {
-
+    #clear the existing inverse 
+    i <- NULL
+    
+    #clear the existing data and inverse
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    #set inverse value
+    setInverse <- function(inverse) i <<- inverse
+    getInverse <- function() i
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+#see if there exists a variable in the environments for i, and if not, solve the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i<-x$getInverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    #retrieve the matrix 
+    data <- x$get() 
+    #solve the matrix using built-in function
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i
 }
